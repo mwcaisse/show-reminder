a3c51184d48df3545c49822815b526c94be9d07a
Mitchell Caisse <computers1055@gmail.com>
2017-02-21 22:19:16 -0500

    Fooling around in Angular

--- src/ShowReminder.Web/wwwroot/app/app.component.ts
+++ src/ShowReminder.Web/wwwroot/app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 
+export class Show {
+    id: number;
+    name: string;
+}
+
+
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello {{name}}</h1>`
+    template: `<h1>Hello {{show.name}}</h1>`
 })
-export class AppComponent { name = 'Spartan'; }
+export class AppComponent {
+    show: Show ={
+        id: 1,
+        name: "Quantico"
+    }
+}
