c12e38e72777029f1da9e3964ba7baf7b29b99ce
Mitchell Caisse <computers1055@gmail.com>
2017-03-01 22:38:54 -0500

    added some promisey http get thingy

--- src/ShowReminder.Web/wwwroot/app/show.service.ts
+++ src/ShowReminder.Web/wwwroot/app/show.service.ts
@@ -1,4 +1,8 @@
 ﻿import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/topromise';
 
 import { Show } from "./show";
 import { SHOWS } from "./mock-shows";
@@ -6,9 +10,20 @@ import { SHOWS } from "./mock-shows";
 @Injectable()
 export class ShowService {
 
+    constructor(private http: Http) {}
+
     getShows(): Promise<Show[]> {
         //we have the Shows in an array, so instantly resolve the promise
-        return Promise.resolve(SHOWS);
+        //return Promise.resolve(SHOWS);
+
+        return this.http.get("lets/put/url/here")
+            .toPromise()
+            .then(this.extractData);
     };
+
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.data || {};
+    }
 
 }
